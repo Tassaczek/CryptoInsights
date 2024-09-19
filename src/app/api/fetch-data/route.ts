d4194084bb41062767334bbd8e49243f191a24d5
Tassak <damian@mythe.pl>
2024-09-19 23:44:37 +0200

    Save gas prices to mongodb

--- src/app/api/fetch-data/route.ts
+++ src/app/api/fetch-data/route.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-
+import clientPromise from '@/lib/mongodb';
 type GasPrice = {
   suggestedMaxPriorityFeePerGas: string;
   suggestedMaxFeePerGas: string;
@@ -53,7 +53,19 @@ async function fetchGasPrices(): Promise<GasPricesResponse> {
 export async function GET(req: NextRequest) {
   try {
     const gasPrices = await fetchGasPrices();
-    return NextResponse.json(gasPrices);
+
+    const client = await clientPromise;
+    const db = client.db();
+    const collection = db.collection('gas_prices');
+
+    await collection.insertOne({
+      ...gasPrices,
+      timestamp: new Date(),
+    });
+    return NextResponse.json({
+      message: 'Gas prices saved successfully',
+      status: 'success',
+    });
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
